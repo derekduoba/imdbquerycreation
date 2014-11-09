012e75e7b966cefaba8aacd0648657481ff19123
dsu1 <dsu@umass.edu>
2014-11-09 17:16:07 -0500

    added our custom sql tables and list rental plans

---
+++ CS345 - Project 2/src/customCreateTables.sql
@@ -0,0 +1,67 @@
+/*
+Login
+uid, username, password
+
+Contact
+cid, first name, last name, phone number, email, address, city, state
+
+Customer
+uid, plantype, recorded rentals, number of rentals, maximum number of rentals
+
+Rentals
+rid, movies, customer uid
+
+PastRentals
+rid, movies, customer uid
+
+Rental plans
+pid, max rentals, fees, duration
+*/
+
+CREATE TABLE LOGIN(
+lid integer FOREIGN KEY REFRENCES Customer (uid),
+username varchar(30),
+password varchar(30)
+);
+
+CREATE TABLE ContactInfo(
+cid integer FOREIGN KEY REFRENCES Customer (uid),
+firstname varChar(30),
+lastname varChar(30),
+email varChar(30),
+address varChar(30),
+city varChar(30),
+state varchar(30)
+);
+
+
+CREATE TABLE Customer(
+uid integer PRIMARY KEY,
+tid varchar(30) FOREIGN KEY REFRENCES RENTALPLANS (pid),
+rid varchar(30) FOREIGN KEY REFRENCES RENTALS (rid),
+number_rented integer,
+max_rentals integer
+);
+
+
+//Multiple instances of similar rid and uid will map to different movies
+CREATE TABLE RENTALS(
+rid integer UNIQUE,
+mid FOREIGN KEY REFRENCES MOVIES (mid),
+uid FOREIGN KEY REFRENCES Customer (uid)
+);
+
+CREATE TABLE RENTALS_HISTORY(
+rhid integer UNIQUE,
+mid FOREIGN KEY REFRENCES MOVIES (mid),
+uid FOREIGN KEY REFRENCES Customer (uid)
+);
+
+//Added the name field
+CREATE TABLE RENTALPLANS(
+pid integer PRIMARY KEY,
+name VARCHAR(50) UNIQUE NOT NULL,
+max_rentals integer,
+cost float,
+months_subscribed integer
+);
